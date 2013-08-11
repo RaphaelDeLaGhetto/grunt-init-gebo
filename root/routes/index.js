9445516f615f1f57d170fa8a23c4d9ab2cbb99bf
Daniel Bidulock <daniel@capitolhill.ca>
2013-08-10 23:35:42 -0600

    Add login views, DB init, and route for auth

--- root/routes/index.js
+++ root/routes/index.js
@@ -2,7 +2,18 @@ module.exports = function(app) {
 
     // Home/main
     app.get('/', function(req, res) {
-        res.render('index', { title: '{%= name %}' })
-    })
+        res.render('index', {
+            title: '{%= name %}',
+            user: req.user
+        });
+    });
 
-}
+    // Login
+    app.get('/login', function(req, res) {
+        res.render('login', {
+            title: '{%= name %}',
+            user: req.user
+        });
+    });
+
+};
