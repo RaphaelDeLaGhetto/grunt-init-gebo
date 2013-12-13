2db5b909834624a994b23b21f7ba8279fee356c4
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-12-13 11:49:36 -0700

    Get index tests

--- root/actions/index.js
+++ root/actions/index.js
@@ -1,79 +1,23 @@
-var utils = require('../lib/utils'),
-    agentSchema = require('../schemata/agent'),
-    q = require('q');
-
 /**
  * Inspired by Greg Wang, 2013-11-5
  * http://stackoverflow.com/questions/5364928/node-js-require-all-files-in-a-folder
  *
- * Load `*.js` under current directory as properties
- * i.e., `User.js` will become `exports['User']` or `exports.User`
+ * This module does not check for duplicate action names. If duplicate names
+ * exist, the former will be overwritten by the latter.
  */
-module.exports = function(email) {
-
-    // Turn the email into a mongo-friendly database name
-    var dbName = utils.ensureDbName(email);
+module.exports = function(gebo) {
 
     require('fs').readdirSync(__dirname + '/').forEach(function(file) {
         if (file.match(/^\w+\.js/g) !== null && file !== 'index.js') {
-          var actions = require('./' + file)(dbName);
+          var actions = require('./' + file)(gebo);
           var keys = Object.keys(actions);
-
           for (var i = 0; i < keys.length; i++) {
             if (!exports[keys[i]]) {
               exports[keys[i]] = actions[keys[i]];
-             // throw 'Two actions cannot have the same name';
             }
           }
         }
       });
-
-    /**
-     * Add a new action to the actions module.
-     *
-     * This function does not check to see if a function
-     * name will overwrite another. That task is left to 
-     * the developer, for now.
-     *
-     * @param string - optional name
-     * @param function or module.exports object
-     */
-   exports.add = function(name, actions) {
-
-        if (typeof name === 'function') {
-          actions = name;
-
-          if (actions.name) {
-            name = actions.name;
-          }
-          else {
-            throw new Error('This action needs a name');
-          }
-          exports[name] = actions; 
-        }
-        else if (typeof name === 'string' && typeof actions === 'function') {
-          exports[name] = actions; 
-        }
-        else if (typeof name === 'object' && !actions) {
-          actions = name;
-          var keys = Object.keys(actions);
-          for (var i = 0; i < keys.length; i++) {
-            if (!exports[keys[i]]) {
-              exports[keys[i]] = actions[keys[i]];
-             // throw 'Two actions cannot have the same name';
-            }
-          }
-        }
-     }; 
-
-   /**
-    * Remove an action by name
-    *
-    * @param string
-    */
-   exports.remove = function(name) {
-        delete exports[name];
-     };
 
     return exports;
   };
