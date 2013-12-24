357eaf19983ace40349ab06af7f22a02b2d2540b
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2013-12-24 10:38:25 -0700

    Match all files ending with .js

--- root/test/schemata/mocks/index.js
+++ root/test/schemata/mocks/index.js
@@ -2,11 +2,11 @@
  * Inspired by Greg Wang, 2013-11-5
  * http://stackoverflow.com/questions/5364928/node-js-require-all-files-in-a-folder
  *
- * This function is identical to that found in the schemata directory
+ * This function should be identical to that found in the schemata directory
  */
 var schema, key;
 require('fs').readdirSync(__dirname + '/').forEach(function(file) {
-    if (file.match(/^\w+\.js/g) !== null && file !== 'index.js') {
+    if (file.match(/^.+\.js$/g) !== null && file !== 'index.js') {
       schema = require('./' + file);
       key = file.replace('.js', '');
       exports[key] = schema;
