f47436bbb4c2591cf429bb497826fa1a651d84af
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2014-03-31 16:16:08 -0600

    Implement clustering

--- root/name.js
+++ root/name.js
@@ -1,8 +1,45 @@
-var gebo = require('gebo-server')(__dirname),
-    actions = require('./actions')(gebo),
-    schemata = require('./schemata');
+//
+//var gebo = require('gebo-server')(__dirname),
+//    actions = require('./actions')(gebo),
+//    schemata = require('./schemata');
+//
+//gebo.schemata.add(schemata);
+//
+//gebo.start();
+//
 
-gebo.schemata.add(schemata);
+var cluster = require('cluster');
 
-gebo.start();
 
+/**
+ * The clustering stuff is courtesy of Rowan Manning
+ * http://rowanmanning.com/posts/node-cluster-and-express/
+ * 2014-2-28
+ */
+if (cluster.isMaster) {
+
+    require('strong-cluster-connect-store').setup();
+
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
+
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+      cluster.fork();
+    }
+
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
+        // Replace the dead worker,
+        // we're not sentimental
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
+      });
+}
+else {
+   var gebo = require('gebo-server')(__dirname);
+   
+   gebo.start();
+
+   console.log('Worker ' + cluster.worker.id + ' running!');
+}
