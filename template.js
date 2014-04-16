60d2805616e46c83530fc85f7b9a0636e1748fbe
Daniel Bidulock <dan.bidulock@hiregroundsoftware.com>
2014-04-16 10:01:29 -0600

    Tighten strong-cluster-express-store version dependency

--- template.js
+++ template.js
@@ -59,7 +59,7 @@ exports.template = function(grunt, init, done) {
                     'mongoose': '*',
                     'nconf': '*',
                     'q': '*',
-                    'strong-cluster-express-store': '*',
+                    'strong-cluster-express-store': '0.0.x',
                 };
             props.devDependencies = {
                     'database-cleaner': '~0.7.0',
